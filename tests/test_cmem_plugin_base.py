09b459265a4f2bfb5fa4cfe4fc7e9e6e7a4ca221
Sebastian Tramp <sebastian.tramp@eccenca.com>
2022-02-22 14:13:45 +0100

    first tested example plugin

--- tests/test_cmem_plugin_base.py
+++ tests/test_cmem_plugin_base.py
@@ -1,5 +1,33 @@
-from cmem_plugin_base import __version__
+"""test file."""
+from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
+from cmem_plugin_base.dataintegration.entity import (
+    Entities,
+    Entity,
+    EntitySchema,
+    EntityPath,
+)
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+class OutputOnlyPlugin(WorkflowPlugin):
+    """Example Plugin - only Output."""
+
+    def __init__(self):
+        pass
+
+    def execute(self, inputs=()) -> Entities:
+        entity1 = Entity(uri="urn:my:1", values=(["value1"], ["value2"]))
+        entity2 = Entity(uri="urn:my:2", values=(["value3"], ["value4"]))
+        schema = EntitySchema(
+            type_uri="urn:my-entity",
+            paths=(EntityPath(path="urn:name"), EntityPath(path="urn:desc")),
+        )
+        return Entities(entities=[entity1, entity2], schema=schema)
+
+
+def test_readonly_plugin():
+    """Test example Workflow Plugin."""
+    output_only = OutputOnlyPlugin()
+    result = output_only.execute()
+    assert len(result.schema.paths) == 2
+    print(type(result.entities[0]))
+    #   [-    assert __version__ == "0.1.0"-]{+__init__(self):+}
