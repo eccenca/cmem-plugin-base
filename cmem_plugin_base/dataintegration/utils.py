373a240d77f0ffc233a1db4f220cabbe04fde4f4
Sebastian Tramp <sebastian.tramp@eccenca.com>
2022-04-07 22:38:19 +0200

    add function to setup cmempy environment

--- cmem_plugin_base/dataintegration/utils.py
+++ cmem_plugin_base/dataintegration/utils.py
@@ -1,4 +1,5 @@
 """Utils for dataintegration plugins."""
+import os
 import re
 
 
@@ -7,3 +8,18 @@ def generate_id(name: str) -> str:
     Characters that are not allowed in an identifier are removed.
     """
     return re.sub(r"[^a-zA-Z0-9_-]", "", name)
+
+
+def setup_cmempy_super_user_access():
+    """Setup environment for accessing CMEM with cmempy."""
+    try:
+        len(os.environ["CMEM_BASE_URI"])
+        os.environ["OAUTH_GRANT_TYPE"] = "client_credentials"
+        os.environ["OAUTH_CLIENT_ID"] = os.environ[
+            "DATAINTEGRATION_CMEM_SERVICE_CLIENT"
+        ]
+        os.environ["OAUTH_CLIENT_SECRET"] = os.environ[
+            "DATAINTEGRATION_CMEM_SERVICE_CLIENT_SECRET"
+        ]
+    except KeyError as error:
+        raise ValueError("Super user configuration not available.") from error
